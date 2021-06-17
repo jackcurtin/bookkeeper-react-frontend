c08b56c22093a0566bbbe839e2a5a99f4765d7ab
Jack Curtin <jackcurtin94@gmail.com>
2021-06-17 14:45:42 -0500

    hero video and text with buttons layout complete

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Button} from "../button/button";
+
 
 function Hero() {
     return (
-        <div>
+        <div className="hero-container">
+            <video src="../../media/hero-vid.mp4" autoPlay loop muted />
+            <h1>FUEL YOUR IMAGINATION</h1>
+            <p>Your next favorite book is just a few clicks away...</p>
+            <div className="hero-btn">
+                <Button className="btn"
+                        buttonStyle="btn--outline"
+                        buttonSize="btn--large">
+                    GET STARTED
+                </Button>
+            </div>
 
         </div>
     );
