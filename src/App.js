d5bf4ac2d31d05ab97846c2bfc8310c3027d137b
Jack Curtin <jackcurtin94@gmail.com>
2021-06-17 14:56:32 -0500

    video moved to public folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Hero from "./components/hero/Hero";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Router>
           <Navbar/>
+          <Hero/>
       </Router>
     </div>
   );
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Button} from "../button/button";
+import "./Hero.css";
+import "../../App.css";
 
 
 function Hero() {
     return (
         <div className="hero-container">
-            <video src="../../media/hero-vid.mp4" autoPlay loop muted />
+            <video src="/videos/hero-vid.mp4" autoPlay loop muted />
             <h1>FUEL YOUR IMAGINATION</h1>
             <p>Your next favorite book is just a few clicks away...</p>
             <div className="hero-btn">
