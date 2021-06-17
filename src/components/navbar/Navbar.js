ab45de7c4ba5f93812c56a88e94b3ee38a91adf9
Jack Curtin <jackcurtin94@gmail.com>
2021-06-17 13:03:44 -0500

    navbar items and click handlers added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,44 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 
 function Navbar () {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <React.Fragment>
             <nav className="navbar">
                 <Link to="/" className="navbar-logo">
-                    Bookkeeper <i className="fa-solid fa-book"></i>
+                    Bookkeeper <i className="fas fa-book"></i>
                 </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                </div>
             </nav>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                        Library
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                        Contribute
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                        Login
+                    </Link>
+                </li>
+            </ul>
         </React.Fragment>
     );
 }
