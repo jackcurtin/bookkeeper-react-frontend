369bf7ae086d52d878cc6e7f5ea7edf9a2fd8769
Jack Curtin <jackcurtin94@gmail.com>
2021-06-17 13:34:14 -0500

    desktop navbar designed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import "./Navbar.css"
 
 function Navbar () {
     const [click, setClick] = useState(false);
@@ -10,35 +11,37 @@ function Navbar () {
     return (
         <React.Fragment>
             <nav className="navbar">
-                <Link to="/" className="navbar-logo">
-                    Bookkeeper <i className="fas fa-book"></i>
-                </Link>
-                <div className="menu-icon" onClick={handleClick}>
-                    <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        Bookkeeper <i className="fas fa-book"></i>
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                    </div>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Library
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Contribute
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-                <li className="nav-item">
-                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                        Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                        Library
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                        Contribute
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                        Login
-                    </Link>
-                </li>
-            </ul>
         </React.Fragment>
     );
 }
