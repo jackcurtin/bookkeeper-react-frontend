c01570bb4b84c06d6b558991804c7f79e565ad39
Jack Curtin <jackcurtin94@gmail.com>
2021-06-18 15:16:37 -0500

    need to reassess back end before using AJAX - setting up basic CSS for now

--- src/components/pages/library/LibraryBookCards.js
+++ src/components/pages/library/LibraryBookCards.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import LibraryItemCard from "./LibraryItemCard";
 
 function LibraryBookCards(props) {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch("https://bookkeeperdb.herokuapp.com/api/books")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setBooks(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
     return (
         <div className="library-book-cards">
             <div className="library-book-cards-container">
@@ -12,6 +31,18 @@ function LibraryBookCards(props) {
                                   cat1="Cormac McCarthy"
                                   cat2="Western"
                                   cat3="Random House">
+                        </LibraryItemCard>
+                        <LibraryItemCard className="library-book-cards-item"
+                                         head="A Wizard of Earthsea"
+                                         cat1="Ursula K. LeGuin"
+                                         cat2="Fantasy"
+                                         cat3="Bantam Books">
+                        </LibraryItemCard>
+                        <LibraryItemCard className="library-book-cards-item"
+                                         head="A Wild Sheep Chase"
+                                         cat1="Haruki Murakami"
+                                         cat2="Mystery"
+                                         cat3="Vintage Books">
                         </LibraryItemCard>
                     </ul>
                 </div>
