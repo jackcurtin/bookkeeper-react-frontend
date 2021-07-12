f7cdb272c3e46987b5c06f36674448356f32a330
Jack Curtin <jackcurtin94@gmail.com>
2021-07-12 13:59:26 -0500

    changed photo and video sources for AWS

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,7 +7,7 @@ import "../../App.css";
 function Hero() {
     return (
         <div className="hero-container">
-            <video src="/videos/hero-vid.mp4" autoPlay loop muted />
+            <video src="https://elasticbeanstalk-us-east-2-911671307707.s3.us-east-2.amazonaws.com/hero-vid.mp4" autoPlay loop muted />
             <h1>FUEL YOUR IMAGINATION</h1>
             <p>Your next favorite book is just a few clicks away...</p>
             <div className="hero-btn">
